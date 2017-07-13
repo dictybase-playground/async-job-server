d377e25da99109b386eb5ed3bdd47c187e77f402
Rashad Laher <rashadlaher@gmail.com>
2017-07-13 18:54:30 -0400

    exec command runs blast program

--- worker.go
+++ worker.go
@@ -5,14 +5,14 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
-	"strings"
 )
 
 //blastp -db dicty_primary_protein -query test_query.fsa
 // -evalue 0.1 -num_alignments 50 -word_size 3 -seg 'yes'
 func main() {
-	cmd := exec.Command("tr", "-s", "s")
-	cmd.Stdin = strings.NewReader("sssss input")
+	// cmd := exec.Command("tr", "-s", "s")
+	// cmd.Stdin = strings.NewReader("sssss input")
+	cmd := exec.Command("blastp", "-db", "dicty_primary_protein", "-query", "test_query.fsa")
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	err := cmd.Run()
